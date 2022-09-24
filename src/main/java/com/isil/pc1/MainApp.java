c2dc1ba197c8e2ae29610e098f96d26ba25b8d9d
Guillermo Tolentino <guillermotolentino23@gmail.com>
2022-09-24 14:13:13 -0500

    Creando la base de datos conectamos con el servidor y creamos el Statement y consultamos

---
+++ src/main/java/com/isil/pc1/MainApp.java
@@ -0,0 +1,33 @@
+package com.isil.pc1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class MainApp {
+    public static  void main(String[] args) throws Exception{
+ Class.forName("com.mysql.cj.jdbc.Driver");
+ 
+ Connection con = DriverManager.getConnection(
+         "jdbc:mysql://localhost:3306/hotelpc1",
+         "root",
+         "");
+
+    Statement Stmt= con.createStatement();
+
+    ResultSet result= Stmt.executeQuery("select * from clientes");
+
+    while (result.next()){
+        System.out.println(result.getString("idCli"));
+        System.out.println(result.getString("paisCli"));
+        System.out.println(result.getString("nombreCli"));
+        System.out.println(result.getString("apellidoCli"));
+        System.out.println(result.getString("direccionCli"));
+        System.out.println(result.getString("telefonoCli"));
+    }
+
+    }
+
+}
